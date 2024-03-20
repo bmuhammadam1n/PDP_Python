d47d4b47fb228890e235e038e3964b4dd4de2824
bmuhammadam1n <muhammadaminboymuxamedov@gmail.com>
2024-03-20 21:02:55 +0500

    update

--- pdp_homework/5masala_to'plami/6masala.py
+++ pdp_homework/5masala_to'plami/6masala.py
@@ -1,4 +1,5 @@
 narx = float(input("1 kg konfetning narxini kiriting: "))
-print("0.1, 0.2, ..., 1 kg konfetning baholari:")
-for kg in range(1, 11):
+print("1, 1.2, ..., 2 kg konfetning baholari:")
+for kg in range(10, 21, 2):
     print(f"{kg / 10} kg uchun: {narx * (kg / 10)} so'm")
+
---
+++ pdp_homework/6masala_to'plami/9masala.py
@@ -0,0 +1,7 @@
+n = int(input("n ni kiriting: "))
+
+k = 1
+while 3 ** k <= n:
+    k += 1
+
+print("3k > n tengsizlik o'rnli eng kichik k:", k)
