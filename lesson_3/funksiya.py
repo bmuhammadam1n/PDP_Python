8cbd8ad54510e420920481cf355c0fff9d0fbb05
bmuhammadam1n <muhammadaminboymuxamedov@gmail.com>
2024-03-25 15:38:56 +0500

    update

---
+++ lesson_3/funksiya.py
@@ -0,0 +1,31 @@
+# def calc (a,b):
+#     return a+b
+# print(calc(5,7))
+
+
+# arr = ["olma", "anor", "gilos", "nik"]
+# def arr1 (arr):
+#     for i in arr:
+#       print(i)
+# arr1(arr)
+
+
+# teacher = []
+# student = []
+#
+# def check_name(name):
+#     if name.isalpha():
+#         return name
+#     else:
+#         print("xato qiymat")
+#         return "default name"
+# for i in range(4):
+#     full_name = input("ism kiriting: ")
+#     if i <= 1:
+#         teacher.append(check_name(full_name))
+#     if i >= 2:
+#         student.append(check_name(full_name))
+# print(teacher)
+# print(student)
+
+
---
+++ lesson_3/while.py
@@ -0,0 +1,47 @@
+
+# true_pass = "pdp_student"
+# while True:
+#     inp_pass = input("parol kiriting")
+#     if inp_pass == true_pass:
+#         print("tizimga kirildi")
+#         break
+#     else:
+
+
+# true_pass = input("parol qoying: ")
+# counter = 0
+# while True:
+#     if counter == 3:
+#         print("yana ikkita imkoniyat qoldi")
+#     elif counter == 5:
+#         print("siz bloklandingiz!!!")
+#         break
+#     inp_pass = input("parol kiriting")
+#     if inp_pass == true_pass:
+#         print("tizimga kirildi")
+#         break
+#     else:
+#         print("qayta uruning")
+#     counter += 1
+
+
+# txt = []
+# num_1 = []
+# bool_true = []
+# bool_false = []
+# while True:
+#     matn = input("matn kiriting: ")
+#     if matn.lower() == "stop":
+#         break
+#     elif matn.lower() == "true":
+#         bool_true.append(matn)
+#     elif matn.lower() == "false":
+#         bool_false.append(matn)
+#     elif matn.isalpha():
+#         txt.append(matn)
+#     elif matn.isdigit():
+#         num_1.append(matn)
+#     else:
+#         print("xato")
+# print(f"{txt}\n{num_1}\n{bool_false}\n{bool_true}")
+
